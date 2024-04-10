dcaa44600681dcda84b27f634056edba2c049c18
boriskiva <boriskiva3@gmail.com>
2024-04-10 19:29:30 +0300

    making default app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TopBarComponent} from "./comps/top-bar/top-bar.component";
+import {SlideMenuComponent} from "./comps/_models/slide-menu/slide-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent, SlideMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
